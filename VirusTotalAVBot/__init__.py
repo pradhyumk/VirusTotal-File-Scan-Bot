791650a59411227ded26ce3eea3b3128e923719f
Pradhyum Krishnan <pradhyum.krishnan@rutgers.edu>
2020-11-01 12:16:07 -0500

    Update __init__.py

--- VirusTotalAVBot/__init__.py
+++ VirusTotalAVBot/__init__.py
@@ -3,5 +3,7 @@ import os
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 API_ID = int(os.environ.get('API_ID'))
 API_HASH = os.environ.get('API_HASH')
+BOT_USERNAME = os.environ.get('BOT_USERNAME')
 PLUG_IN = dict(root="VirusTotalAVBot.modules")
 VT_API = os.environ.get('VT_API')
+GROUP_INFO_MSGS = os.environ.get('GROUP_INFO_MSGS')
